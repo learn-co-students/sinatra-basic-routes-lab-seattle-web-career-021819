c4970235fa7cf2bd2243fd32ce6ecfb2bd61b939
clairemuller <clairermuller@gmail.com>
2019-03-11 13:22:46 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Claire."
+  end
+
+  get '/hometown' do
+    "My hometown is Mansfield."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is SONG."
+  end
+
 end
